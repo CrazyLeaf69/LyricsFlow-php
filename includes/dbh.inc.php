a1eb7152298edc4505a791a7831ade61f7cd3796
Lowe Lowing <lowe.lowing@gmail.com>
2021-05-05 18:49:24 +0200

    remote db connection

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,16 @@
 <?php
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "lyricsflow_php";
+// Development Connection
+// $serverName = "localhost";
+// $dBUsername = "root";
+// $dBPassword = "";
+// $dBName = "lyricsflow_php";
+
+// Remote Database Connection
+$serverName = "remotemysql.com";
+$dBUsername = "WnjUQDzsw7";
+$dBPassword = "eYbm0MqX1l";
+$dBName = "WnjUQDzsw7";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
