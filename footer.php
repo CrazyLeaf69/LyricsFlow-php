44fd8e0db723c7a9519666c321035bd4bc835373
CrazyLeaf69 <76205967+CrazyLeaf69@users.noreply.github.com>
2021-05-06 11:01:39 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
     <p>Made By: Lowe Löwing <a href="mailto: lowe.lowing@gmail.com"><i class="fas fa-envelope"></i></p></a>
-    <p>© 2020 Copyright: LyricsFlow.net</p>
+    <p>© 2021 Copyright: LyricsFlow.net</p>
 </div>
 <script src="javascript/active.js"></script>
