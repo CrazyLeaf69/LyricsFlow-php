845ba010270fbb65487f49153e67a4208b4a6ba0
CrazyLeaf69 <76205967+CrazyLeaf69@users.noreply.github.com>
2022-08-31 17:57:08 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,10 @@
 <?php
 
 // Development Connection
-// $serverName = "localhost";
-// $dBUsername = "root";
-// $dBPassword = "";
-// $dBName = "lyricsflow_php";
-
-// Remote Database Connection
-$serverName = "remotemysql.com";
-$dBUsername = "WnjUQDzsw7";
-$dBPassword = "eYbm0MqX1l";
-$dBName = "WnjUQDzsw7";
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "lyricsflow_php";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
